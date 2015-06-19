eacf4b60728f057d5fd2b417a0534d6f34fb9642
Tom Bailey <tombailey1@hotmail.co.uk>
2015-06-19 15:45:34 +0100

    Bug fix
    0th term treated as 1st term rather than underflowing as before.

--- Fibo.c
+++ Fibo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 unsigned int fibo(unsigned int n) {
-	return (n == 1 || n == 2) ? 1 : fibo(n-1) + fibo(n-2);
+	return (n <= 2) ? 1 : fibo(n-1) + fibo(n-2);
 }
 
 int main(void) {
